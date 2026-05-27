btwiuse/crush

boba: switch to boba.{Program,NewProgram} API

internal/app/app.go
@@ -18,6 +18,7 @@ import (
 	"charm.land/catwalk/pkg/catwalk"
 	"charm.land/fantasy"
 	"charm.land/lipgloss/v2"
+	"github.com/btwiuse/boba"
 	"github.com/charmbracelet/crush/internal/agent"
 	"github.com/charmbracelet/crush/internal/agent/notify"
 	"github.com/charmbracelet/crush/internal/agent/tools/mcp"
@@ -539,7 +540,7 @@ func (app *App) InitCoderAgent(ctx context.Context) error {
 }
 
 // Subscribe sends events to the TUI as tea.Msgs.
-func (app *App) Subscribe(program *tea.Program) {
+func (app *App) Subscribe(program boba.Program) {
 	defer log.RecoverPanic("app.Subscribe", func() {
 		slog.Info("TUI subscription panic: attempting graceful shutdown")
 		program.Quit()

internal/cmd/root.go
@@ -18,6 +18,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/btwiuse/boba"
 	tea "charm.land/bubbletea/v2"
 	fang "charm.land/fang/v2"
 	"charm.land/lipgloss/v2"
@@ -123,7 +124,7 @@ crush --continue
 		model := ui.New(com, sessionID, continueLast)
 
 		var env uv.Environ = os.Environ()
-		program := tea.NewProgram(
+		program := boba.NewProgram(
 			model,
 			tea.WithEnvironment(env),
 			tea.WithContext(cmd.Context()),

internal/format/spinner.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	tea "charm.land/bubbletea/v2"
+	"github.com/btwiuse/boba"
 	"github.com/charmbracelet/crush/internal/ui/anim"
 	"github.com/charmbracelet/x/ansi"
 )
@@ -14,7 +15,7 @@ import (
 // Spinner wraps the bubbles spinner for non-interactive mode
 type Spinner struct {
 	done chan struct{}
-	prog *tea.Program
+	prog boba.Program
 }
 
 type model struct {
@@ -48,7 +49,7 @@ func NewSpinner(ctx context.Context, cancel context.CancelFunc, animSettings ani
 		cancel: cancel,
 	}
 
-	p := tea.NewProgram(m, tea.WithOutput(os.Stderr), tea.WithContext(ctx))
+	p := boba.NewProgram(m, tea.WithOutput(os.Stderr), tea.WithContext(ctx))
 
 	return &Spinner{
 		prog: p,

internal/workspace/app_workspace.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"time"
 
-	tea "charm.land/bubbletea/v2"
+	"github.com/btwiuse/boba"
 	"github.com/charmbracelet/crush/internal/agent"
 	mcptools "github.com/charmbracelet/crush/internal/agent/tools/mcp"
 	"github.com/charmbracelet/crush/internal/app"
@@ -367,7 +367,7 @@ func (w *AppWorkspace) DisableDockerMCP() error {
 
 // -- Lifecycle --
 
-func (w *AppWorkspace) Subscribe(program *tea.Program) {
+func (w *AppWorkspace) Subscribe(program boba.Program) {
 	w.app.Subscribe(program)
 }
 

internal/workspace/client_workspace.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	tea "charm.land/bubbletea/v2"
+	"github.com/btwiuse/boba"
 	"github.com/charmbracelet/crush/internal/agent/notify"
 	"github.com/charmbracelet/crush/internal/agent/tools/mcp"
 	"github.com/charmbracelet/crush/internal/client"
@@ -539,7 +540,7 @@ func (w *ClientWorkspace) DisableDockerMCP() error {
 
 // -- Lifecycle --
 
-func (w *ClientWorkspace) Subscribe(program *tea.Program) {
+func (w *ClientWorkspace) Subscribe(program boba.Program) {
 	defer log.RecoverPanic("ClientWorkspace.Subscribe", func() {
 		slog.Info("TUI subscription panic: attempting graceful shutdown")
 		program.Quit()

internal/workspace/workspace.go
@@ -8,8 +8,8 @@ import (
 	"context"
 	"time"
 
-	tea "charm.land/bubbletea/v2"
 	"charm.land/catwalk/pkg/catwalk"
+	"github.com/btwiuse/boba"
 	mcptools "github.com/charmbracelet/crush/internal/agent/tools/mcp"
 	"github.com/charmbracelet/crush/internal/config"
 	"github.com/charmbracelet/crush/internal/history"
@@ -139,7 +139,7 @@ type Workspace interface {
 	DisableDockerMCP() error
 
 	// Events
-	Subscribe(program *tea.Program)
+	Subscribe(program boba.Program)
 	Shutdown()
 }
 

ohmygate/ohmygate.github.io

internal/app/app.go
@@ -18,6 +18,7 @@ import (
 	"charm.land/catwalk/pkg/catwalk"
 	"charm.land/fantasy"
 	"charm.land/lipgloss/v2"
+	"github.com/justwasm/boba"
 	"github.com/charmbracelet/crush/internal/agent"
 	"github.com/charmbracelet/crush/internal/agent/notify"
 	"github.com/charmbracelet/crush/internal/agent/tools/mcp"
@@ -550,7 +551,7 @@ func (app *App) InitCoderAgent(ctx context.Context) error {
 }
 
 // Subscribe sends events to the TUI as tea.Msgs.
-func (app *App) Subscribe(program *tea.Program) {
+func (app *App) Subscribe(program boba.Program) {
 	defer log.RecoverPanic("app.Subscribe", func() {
 		slog.Info("TUI subscription panic: attempting graceful shutdown")
 		program.Quit()

internal/cmd/root.go
@@ -20,6 +20,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/justwasm/boba"
 	tea "charm.land/bubbletea/v2"
 	fang "charm.land/fang/v2"
 	"charm.land/lipgloss/v2"
@@ -127,7 +128,7 @@ crush --continue
 		model := ui.New(com, sessionID, continueLast)
 
 		var env uv.Environ = os.Environ()
-		program := tea.NewProgram(
+		program := boba.NewProgram(
 			model,
 			tea.WithEnvironment(env),
 			tea.WithContext(cmd.Context()),

internal/format/spinner.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	tea "charm.land/bubbletea/v2"
+	"github.com/justwasm/boba"
 	"github.com/charmbracelet/crush/internal/ui/anim"
 	"github.com/charmbracelet/x/ansi"
 )
@@ -14,7 +15,7 @@ import (
 // Spinner wraps the bubbles spinner for non-interactive mode
 type Spinner struct {
 	done chan struct{}
-	prog *tea.Program
+	prog boba.Program
 }
 
 type model struct {
@@ -48,7 +49,7 @@ func NewSpinner(ctx context.Context, cancel context.CancelFunc, animSettings ani
 		cancel: cancel,
 	}
 
-	p := tea.NewProgram(m, tea.WithOutput(os.Stderr), tea.WithContext(ctx))
+	p := boba.NewProgram(m, tea.WithOutput(os.Stderr), tea.WithContext(ctx))
 
 	return &Spinner{
 		prog: p,

internal/workspace/app_workspace.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"time"
 
-	tea "charm.land/bubbletea/v2"
+	"github.com/justwasm/boba"
 	"github.com/charmbracelet/crush/internal/agent"
 	mcptools "github.com/charmbracelet/crush/internal/agent/tools/mcp"
 	"github.com/charmbracelet/crush/internal/app"
@@ -391,7 +391,7 @@ func (w *AppWorkspace) DisableDockerMCP() error {
 
 // -- Lifecycle --
 
-func (w *AppWorkspace) Subscribe(program *tea.Program) {
+func (w *AppWorkspace) Subscribe(program boba.Program) {
 	w.app.Subscribe(program)
 }
 

internal/workspace/client_workspace.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	tea "charm.land/bubbletea/v2"
+	"github.com/justwasm/boba"
 	"github.com/charmbracelet/crush/internal/agent/notify"
 	"github.com/charmbracelet/crush/internal/agent/tools/mcp"
 	"github.com/charmbracelet/crush/internal/client"
@@ -592,7 +593,7 @@ func (w *ClientWorkspace) DisableDockerMCP() error {
 
 // -- Lifecycle --
 
-func (w *ClientWorkspace) Subscribe(program *tea.Program) {
+func (w *ClientWorkspace) Subscribe(program boba.Program) {
 	defer log.RecoverPanic("ClientWorkspace.Subscribe", func() {
 		slog.Info("TUI subscription panic: attempting graceful shutdown")
 		program.Quit()

internal/workspace/workspace.go
@@ -8,8 +8,8 @@ import (
 	"context"
 	"time"
 
-	tea "charm.land/bubbletea/v2"
 	"charm.land/catwalk/pkg/catwalk"
+	"github.com/justwasm/boba"
 	mcptools "github.com/charmbracelet/crush/internal/agent/tools/mcp"
 	"github.com/charmbracelet/crush/internal/config"
 	"github.com/charmbracelet/crush/internal/history"
@@ -156,7 +156,7 @@ type Workspace interface {
 	DisableDockerMCP() error
 
 	// Events
-	Subscribe(program *tea.Program)
+	Subscribe(program boba.Program)
 	Shutdown()
 }
 